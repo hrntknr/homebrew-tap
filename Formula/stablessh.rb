d0439d6ee5fa588482c5f7dd4eb6a08bdd6601e5
Takanori Hirano <me@hrntknr.net>
2024-03-23 19:24:41 +0900

    stablessh v0.1.3

--- Formula/stablessh.rb
+++ Formula/stablessh.rb
@@ -1,8 +1,8 @@
 class Stablessh < Formula
   desc "Keeps SSH on even when laptop is closed"
   homepage "https://github.com/hrntknr/stablessh"
-  url "https://github.com/hrntknr/stablessh/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "df6393edf6908a707435bd413d67367e71098e3f7df722c50939330ce7e65c44"
+  url "https://github.com/hrntknr/stablessh/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "ef42ea5483a0ec60eb67a41f012628e1dcabde1401834bbb6fbacfe5942a8833"
   license "MIT"
 
   depends_on "rust" => :build
